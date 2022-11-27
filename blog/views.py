106ba9d788e2da22b3f8fe7135d2006c43b8aafd
Jahid Imran <jahidimran21cde@gmail.com>
2022-11-28 03:05:55 +0600

    create frontend structure

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
-# Create your views here.
+# home
+def home(request):
+    return render(request, 'blog/home.html')
+
+# contact
+def contact(request):
+    return render(request, 'blog/contact.html')
+
+# dashboard
+def dashboard(request):
+    return render(request, 'blog/dashboard.html')
+
+# signup
+def signup(request):
+    return render(request, 'blog/signup.html')
+
+# login
+def user_login(request):
+    return render(request, 'blog/login.html')
+
+# logout
+def user_logout(request):
+    return HttpResponseRedirect('/')
